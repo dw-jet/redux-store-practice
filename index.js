334f17bf915f08f1ed6142f8124d67ce15d2cd38
James Teitsworth <james@jamesteitsworth.com>
2020-06-29 15:11:49 -0500

    Add action creators and constants for action types

--- index.js
+++ index.js
@@ -30,13 +30,54 @@ function createStore(reducer) {
 }
 
 // App code
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id
+  }
+}
+
 function todos (state = [], action) {
   switch(action.type) {
-    case 'ADD_TODO' :
+    case ADD_TODO :
       return state.concat([action.todo]);
-    case 'REMOVE_TODO' :
+    case REMOVE_TODO :
       return state.filter((todo) => todo.id !== action.id);
-    case 'TOGGLE_TODO' :
+    case TOGGLE_TODO :
       return state.map((todo) => todo.id !== action.id ? todo :
       Object.assign({}, todo, { complete: !todo.complete }));
     default :
@@ -46,9 +87,9 @@ function todos (state = [], action) {
 
 function goals (state = [], action) {
   switch(action.type) {
-    case 'ADD_GOAL' :
+    case ADD_GOAL :
       return state.concat([action.goal]);
-    case 'REMOVE_GOAL' :
+    case REMOVE_GOAL :
       return state.filter((goal) => goal.id !== action.id);
     default :
       return state;
@@ -68,38 +109,25 @@ store.subscribe(() => {
   console.log('The new state is: ', store.getState())
 })
 
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
+store.dispatch(addTodoAction({
     id: 0,
     name: 'Wash car',
     complete: false
   }
-})
+))
 
-store.dispatch({
-  type: 'ADD_TODO',
-  todo: {
+store.dispatch(addTodoAction({
     id: 1,
     name: 'Walk',
     complete: false
   }
-})
+))
 
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0
-})
+store.dispatch(toggleTodoAction(0))
 
-store.dispatch({
-  type: 'REMOVE_TODO',
+store.dispatch(removeTodoAction(0))
+
+store.dispatch(addGoalAction({
   id: 0,
-})
-
-store.dispatch({
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: "Front end web developer"
-  }
-})
+  name: "Front end web developer"
+}))
